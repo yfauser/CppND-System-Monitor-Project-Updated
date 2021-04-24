4dc82e91eb0244a3df0dc6d96103bc6170395f1b
Yves Fauser <yfauser@yahoo.de>
2021-04-24 16:29:11 +0200

    Completed Process Counts & Uptime including time format

--- include/format.h
+++ include/format.h
@@ -5,6 +5,7 @@
 
 namespace Format {
 std::string ElapsedTime(long times);  // TODO: See src/format.cpp
+std::string TwoDigits(std::string const &timestr);
 };                                    // namespace Format
 
 #endif
--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,35 @@
 #include "format.h"
 
 #include <string>
+#include <chrono>
+#include <sstream>
 
 using std::string;
+using std::chrono::duration_cast;
+using std::chrono::hours;
+using std::chrono::minutes;
 
-// TODO: Complete this helper function
+// Function to format the time to two digits, e.g. 3 sec/min/hours
+// will be returned as 03 seconds
+string Format::TwoDigits(string const &timestr) {
+    if (timestr.length() == 1) {
+        return "0" + timestr;
+    }
+    else return timestr;
+}
+
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+// Inspired by https://stackoverflow.com/questions/42866524/convert-chronoduration-to-string-or-c-string
+string Format::ElapsedTime(long seconds) {
+    std::chrono::seconds sec(seconds);
+    std::ostringstream secstream, minstream, hourstream;
+    hourstream << duration_cast<hours>(sec).count();
+    minstream << duration_cast<minutes>(sec).count() % 60;
+    secstream << sec.count() % 60;
+    string secs = secstream.str();
+    string mins = minstream.str();
+    string hours = hourstream.str();
+    return TwoDigits(hours) + ":" + TwoDigits(mins) + ":" + TwoDigits(secs);
+}
