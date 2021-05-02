3ee5cff78899e1a3f29f940070a650ebd548e6e6
Yves Fauser <yfauser@yahoo.de>
2021-05-02 20:33:35 +0200

    Removed not needed class methods

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -129,19 +129,6 @@ long LinuxParser::UpTime() {
   return stol(filecontent[0][0]);
 }
 
-// TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
-
-// TODO: Read and return the number of active jiffies for a PID
-// REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::ActiveJiffies(int pid [[maybe_unused]]) { return 0; }
-
-// TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
-
-// TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
-
 // DONE: Read and return CPU utilization
 map<string, long> LinuxParser::CpuUtilization() {
   map<string, long> cpustats;
